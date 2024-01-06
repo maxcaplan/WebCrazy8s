821a4f460f1b891dea060e6b0922d42d4f7faeb8
maxcaplan <maxacaplan@gmail.com>
2024-01-06 12:54:08 -0400

    Initial WebSocket server added

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello via Bun!");
+import { ServerWebSocket } from "bun";
+
+// Top level server function
+function main() {
+  console.log("Starting websocket server...");
+
+  const wss = Bun.serve({
+    fetch(req, server) {
+      // Upgrade the request to a WebSocket
+      if (server.upgrade(req)) return;
+      // Failed to upgrade to a Websocket
+      return new Response("Upgrade failed :(", { status: 500 });
+    },
+
+    websocket: {
+      open(ws: ServerWebSocket) {
+        console.log(
+          "Websocket connection opened with address " + ws.remoteAddress,
+        );
+      },
+
+      close(ws: ServerWebSocket) {
+        console.log(
+          "Websocket connection closed with address " + ws.remoteAddress,
+        );
+      },
+
+      message(ws: ServerWebSocket, message: string | Buffer) {
+        ws.send("Echo: " + message);
+      },
+    },
+
+    port: Bun.env.WSS_PORT || 3000,
+  });
+
+  console.log(`Started WebSocket server at ${wss.hostname}:${wss.port}`);
+}
+
+// Start server
+main();
